0f2cbd854fb65b780f559ed5222ceb3aa4412893
GeoSauer <geoffrey.sauer89@gmail.com>
2023-09-18 16:36:11 -0600

    add some styling to posts

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,24 +3,24 @@ import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 
+import utilStyles from "../../styles/utils.module.css";
+
 export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
-        //this makes the post title render on the browser tab
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
-
 export async function getStaticPaths() {
   const paths = getAllPostIds();
   return {
